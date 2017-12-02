13c5daa464b706363a8462cec635b0ec702e6bd6
korikori10 <33927361+korikori10@users.noreply.github.com>
2017-12-02 18:59:05 +0200

    try
    nothing change

--- product-server/App_Code/Product1.cs
+++ product-server/App_Code/Product1.cs
@@ -10,21 +10,21 @@ using System.Web.UI.WebControls;
 /// </summary>
 public class Product1
 {
-    //string name;
-    //string description;
-    //string price;
-    //string category;
+    string name;
+    string description;
+    string price;
+    string category;
     List<Product> newList = new List<Product>();
     Product p = new Product();
-    //public Product1(string name, string price, string category, string description)
-    //{
-    //    this.name = name;
-    //    double P1 = (Convert.ToDouble(price)) * 1.17;
-    //    this.price = P1.ToString();
-    //    this.category = category;
-    //    this.description = description;
+    public Product1(string name, string price, string category, string description)
+    {
+        this.name = name;
+        double P1 = (Convert.ToDouble(price)) * 1.17;
+        this.price = P1.ToString();
+        this.category = category;
+        this.description = description;
 
-    //}
+    }
 
     public void listProducts(List<Product> pl, PlaceHolder ph, bool cbst)
     {
@@ -91,9 +91,9 @@ public class Product1
             }
         }
     }
-    //public string GetInfo()
-    //{
-        
-    //    return "A new product got in! "+ "<br />"+" Name: " + name +"<br />" + "Cost:" + price + " shekels" + "<br />" + "Under the category of:" + category + "<br />"+"Description: " +description+ ".";
-    //}
+    public string GetInfo()
+    {
+
+        return "A new product got in! " + "<br />" + " Name: " + name + "<br />" + "Cost:" + price + " shekels" + "<br />" + "Under the category of:" + category + "<br />" + "Description: " + description + ".";
+    }
 }
